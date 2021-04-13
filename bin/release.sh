4441a00533437b7e7597c7bae833fd1ce7f66f9a
Robert Egglestone <robertegglestone@gmail.com>
2021-04-14 03:32:18 +1200

    Reenable

--- bin/release.sh
+++ bin/release.sh
@@ -11,10 +11,10 @@ git add -f dist/index.js
 git commit -m Release
 
 # replace the tag
-git tag -d debug || true
-git push --delete origin debug || true
-git tag debug
-git push --tags origin debug
+git tag -d v1 || true
+git push --delete origin v1 || true
+git tag v1
+git push --tags origin v1
 
 # restore
 git switch main
