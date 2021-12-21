613fd5baa99056926c381b6ef77f42e7aefba374
amandal-cn <69103902+amandal-cn@users.noreply.github.com>
2021-12-21 21:26:15 +0530

    Work with apt and yum (#3)
    * support apt and yum
    
    * fixed: install epel
    
    * work with apt and yum
    
    * replaced main with work-without-apt branch
    
    * moved install rdfind command to package.json
    
    * add package.json to dist folder
    
    * use child_process instead of @actions/exec
    
    * use await
    
    * test on ubuntu-latest
    
    * exec -> execSync
    
    * reverted back temp changes
    
    * minor change
    
    Co-authored-by: Arleif Braganza <arleif.braganza@devfactory.com>

--- bin/release.sh
+++ bin/release.sh
@@ -11,10 +11,10 @@ git add -f dist/index.js
 git commit -m Release
 
 # replace the tag
-git tag -d v1 || true
-git push --delete origin v1 || true
-git tag v1
-git push --tags origin v1
+git tag -d v2 || true
+git push --delete origin v2 || true
+git tag v2
+git push --tags origin v2
 
 # restore
 git switch main
